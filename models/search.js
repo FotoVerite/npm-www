1ed098714c14e660920d838d90f635b193a77c45
Matthew Bergman <mzbphoto@gmail.com>
2012-11-22 16:41:14 -0500

    slights improvements to how search ranks are done

--- models/search.js
+++ models/search.js
@@ -3,10 +3,11 @@ module.exports = search
 var transform = require('./showprofile.js').transform
 
 function search (params, cb) {
-  var options = {query: {}}
+  var options = {query: { query_string : {} }}
   options.from = ((params.page - 1) * 10)
-  options.fields = ["name", "description"]
-  options.query.field = {"_all": params.q || '*'}
+  options.fields = ["name", "description", 'maintainers']
+  options.query.query_string.fields = ["description", "name^5", "author.name^20", "maintainers.name", "readme"]
+  options.query.query_string.query = params.q || '*'
   client.searchRegistry(options, function(data) {
     return cb(null, JSON.parse(data))
   })
